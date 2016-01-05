ce9c5a620173a888db7d1e3ec6d44b5e0034d95b
Kurt Sparks <sparkys1stp@gmail.com>
2016-01-05 09:13:45 -0600

    cleaned up gitignore, unused directives

--- EmailApp/Services/EmailService.cs
+++ EmailApp/Services/EmailService.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Net.Mail;
-using System.Web.Mvc;
-using System.Web.Routing;
+﻿using System.Net.Mail;
 using EmailApp.Models;
 using Mvc.Mailer;
 
@@ -16,18 +13,18 @@ namespace EmailApp.Services
     {
         public void SendEmail(EmailMessageVM emailMessage)
         {
-                ViewData.Model = emailMessage;
+            ViewData.Model = emailMessage;
 
-                var mvcMailMessage = Populate(x =>
-                {
-                    x.From = new MailAddress(emailMessage.FromEmail);
-                    x.Subject = emailMessage.Subject;
-                    x.ViewName = "Email";
-                    x.MasterName = "_EmailLayout";
-                    x.To.Add(emailMessage.ToEmail);
-                });
+            var mvcMailMessage = Populate(x =>
+            {
+                x.From = new MailAddress(emailMessage.FromEmail);
+                x.Subject = emailMessage.Subject;
+                x.ViewName = "Email";
+                x.MasterName = "_EmailLayout";
+                x.To.Add(emailMessage.ToEmail);
+            });
 
-                mvcMailMessage.Send();
+            mvcMailMessage.Send();
         }
     }
 }
